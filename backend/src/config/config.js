2c0d36a3a88193489653238e6225d79c518e523f
david medina <rafaelsanchez97.dm@gmail.com>
2022-11-12 01:11:39 -0300

    modificacion del archivo config.js como prueba de despliegue en el host de railway

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,8 +1,15 @@
+const puerto = process.env.port || 3000
+
+const DB_HOST =process.env.DB_HOST || "localhost"
+
+const DB_USER =process.env.DB_USER || "root"
+
+const DB_PASSWORD =process.env.DB_PASSWORD || "1234"
+
+const DB_NAME =process.env.DB_NAME || "prueba2"
+
+ const DB_PORT =process.env.DB_PORT || "3306" 
+
 module.exports = {
-
-    database:"prueba2",
-    user:"root",
-    password:"1234",
-    host:"localhost"
-
+    puerto, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT
 }
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,15 @@
 const Sequelize = require("sequelize");
-const baseDeDatos = require("./config");
+const {DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER} = require("./config");
 
 const sequelize = new Sequelize(
-    baseDeDatos.database,
-    baseDeDatos.user,
-    baseDeDatos.password, {
-        host: baseDeDatos.host,
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD, {
+        host: DB_HOST,
         dialect:"mysql",
         define: {timestamps:false}
 
     }
 );
 
-module.exports = sequelize;
+module.exports = sequelize; 
