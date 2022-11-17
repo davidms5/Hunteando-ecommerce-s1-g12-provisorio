46aab85b74da349966ac3985390a0de0e37e52fe
david medina <rafaelsanchez97.dm@gmail.com>
2022-11-17 00:57:19 -0300

    arreglos de bugs en el archivo de config.js

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,4 +1,4 @@
-const puerto = process.env.puerto || 3000
+const PUERTO = process.env.PUERTO || 3000
 
 const DB_HOST =process.env.DB_HOST || "localhost"
 
@@ -11,5 +11,5 @@ const DB_NAME =process.env.DB_NAME || "prueba2"
  const DB_PORT =process.env.DB_PORT || "3306" 
 
 module.exports = {
-    puerto, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT
+    PUERTO, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT
 }
