3e86c0f22a538965b08468cc826c7b17a4065130
david medina <rafaelsanchez97.dm@gmail.com>
2022-11-23 23:40:55 -0300

    terminando endpoint del carrito

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const Sequelize = require("../config/db");
 const path = require("path");
+const productos = require(`../models/producto`)(Sequelize)
 //const router = express.Router()
 
-const inicio = express();
+const inicio = express.Router();
 
 
-inicio.get("/index", (req, res) => {
+inicio.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '../../../frontend/index.html'))
 })
 
-module.exports = inicio;
+productos.findAll({
+    attributes: ['NOMBRE_PRODUCTO','PRECIO_VENTA', 'IMAGEN','ID_PRODUCTO'],
+}).then(valor =>{
+    return (valor);
+})
+
+module.exports = {inicio, productos};
 
 //desde aqui hacer el endpoint de la base de datos al front
 
