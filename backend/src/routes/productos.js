21986ce6feffecd40bd15f04da4aaee5d9161e88
david medina <rafaelsanchez97.dm@gmail.com>
2022-12-08 22:22:25 -0300

    corregido error de los endpoints para traer productos del back al front

--- backend/src/routes/productos.js
+++ backend/src/routes/productos.js
@@ -1,19 +1,42 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
+const sequelize = require("../config/db")
 
-const {producto} = require("../models");
+const ProductoModel = require("../models/producto")(sequelize);
+
 
 //endpoint de producto en especifico
-router.get("/:NOMBRE_PRODUCTO", async(req, res) =>{
+router.get("/:NOMBRE_PRODUCTO", (req, res) =>{
     const productos = req.params.NOMBRE_PRODUCTO;
-    const Producto = await producto.FindAll({where:{ NOMBRE_PRODUCTO: productos }})
-    res.json(Producto);
+
+    ProductoModel.findAll({where:{ NOMBRE_PRODUCTO: productos }}) 
+    .then(datos =>{ 
+        var resultados = JSON.stringify(datos)
+        console.log(resultados) 
+        res.json(resultados)
+    })
+    .catch(error =>{
+        console.log(error)
+    })
+    //const productos = req.params.NOMBRE_PRODUCTO;
+    //const Producto = await ProductoModel.FindAll({where:{ NOMBRE_PRODUCTO: productos }})
+    //res.json(Producto);
 })
 
 //endpoint de query a todos los productos
-router.get("/index", async(req, res) =>{
-    const TotalProducto = await producto.FindAll()
-    res.json(TotalProducto);
+router.get("/", async(req, res) =>{
+    
+    ProductoModel.findAll({attributes:["NOMBRE_PRODUCTO", "IMAGEN", "PRECIO_VENTA", "MARCA"]}) 
+    .then(datos =>{ 
+        var resultados = JSON.stringify(datos)
+        console.log(resultados) 
+        res.json(resultados)
+    })
+    .catch(error =>{
+        console.log(error)
+    })
+   // const TotalProducto = await ProductoModel.FindAll()
+   // res.json(TotalProducto);
 })
 
 module.exports = router;
