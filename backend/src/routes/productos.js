00c227969cf803f62c5742b21c3b99f87fd9d492
david medina <rafaelsanchez97.dm@gmail.com>
2022-12-16 00:45:47 -0300

    creacion del model 'compras', aun por correcion de error de creacion de tabla

--- backend/src/routes/productos.js
+++ backend/src/routes/productos.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const sequelize = require("../config/db")
 
-const ProductoModel = require("../models/producto")(sequelize);
+
+
 
 const {
     getproductos,
