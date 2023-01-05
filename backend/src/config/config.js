2285f3059b3c08d8c46dd1db4b143d0f0c5c8730
david medina <rafaelsanchez97.dm@gmail.com>
2023-01-05 18:22:53 -0300

    arreglos

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,14 +1,14 @@
 const PORT = process.env.PORT || 3000
 
-const DB_HOST =process.env.DB_HOST || "containers-us-west-81.railway.app"//"localhost"
+const DB_HOST =process.env.DB_HOST || "localhost"
 
 const DB_USER =process.env.DB_USER || "root"
 
-const DB_PASSWORD =process.env.DB_PASSWORD || "T3T4K72dkYbaCukhCXkR"//"1234"
+const DB_PASSWORD =process.env.DB_PASSWORD || "1234"
 
 const DB_NAME =process.env.DB_NAME || "railway" // ahora esta como railway/prueba2
 
- const DB_PORT =process.env.DB_PORT || "8047"//"3306" 
+ const DB_PORT =process.env.DB_PORT || "3306" 
 
 module.exports = {
     PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT
